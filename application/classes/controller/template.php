c3533ba99b5ab8813108228b15d979d56f578ae0
Edgars Pelna <regative@gmail.com>
2012-08-27 14:09:46 +0300

    Adjusted code of template controller to phpcs standards

--- application/classes/controller/template.php
+++ application/classes/controller/template.php
@@ -1,12 +1,17 @@
 <?php defined('SYSPATH') or die('Fuck you!');
 
-class Controller_Template extends Kohana_Controller_Template {
+class Controller_Template extends Kohana_Controller_Template
+{
+
     public $template = 'templates/default';
-    public function before(){
+
+    public function before()
+    {
         parent::before();
-        $config = Kohana::$config->load('common');
-        $this->template->site_name = $config->site_name;
+        $config                      = Kohana::$config->load('common');
+        $this->template->site_name   = $config->site_name;
         $this->template->stylesheets = $config->stylesheets;
-        $this->template->scripts = $config->scripts;
+        $this->template->scripts     = $config->scripts;
     }
+
 }
