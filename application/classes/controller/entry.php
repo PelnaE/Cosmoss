f72dcaee5d1d6e64d8526b877eb436ac9399d804
Edgars Pelna <regative@gmail.com>
2012-08-27 14:17:37 +0300

    Adjusting code to phpcs standards - Controller Entry

--- application/classes/controller/entry.php
+++ application/classes/controller/entry.php
@@ -1,21 +1,25 @@
 <?php defined('SYSPATH') or die('Fuck you!');
 
-class Controller_Entry extends Controller_Template{
-    public function action_view(){
-        $entry_id = $this->request->param('id');
-        $slug = $this->request->param('slug');
-        if(!$entry_id){
+class Controller_Entry extends Controller_Template
+{
+    public function action_view()
+    {
+        $entry_id                   = $this->request->param('id');
+        $this->template->page_title = 'Entry #'.$entry_id;
+        $slug                       = $this->request->param('slug');
+
+        if (!$entry_id) {
             throw new Exception('ID must not be empty!');
         }
-        
-        $model_for_entries = Model::factory('entry');
-        $get_entry = $model_for_entries->get_entry_by_id($entry_id);
-        $this->template->page_title = 'Entry #'.$entry_id;
-        if(!$get_entry){
+
+        $entry         = new Model_Entry();
+        $view          = View::factory('entries/view');
+        $view->entries = $entry->get_entry_by_id($entry_id);
+
+        if (!$view->entries) {
             throw new Exception('There is not any entry with this ID!');
         }
-        $view = View::factory('entries/view');
-        $view->entries = $get_entry;
+
         $this->template->content = $view->render();
     }
 }
