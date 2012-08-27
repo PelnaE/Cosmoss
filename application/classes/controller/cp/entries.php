af68fecb4e41a3ee0f8e620f90935f942ae36866
Edgars Pelna <regative@gmail.com>
2012-08-27 12:59:01 +0300

    Code cleanup and adjusting to phpcs standards

--- application/classes/controller/cp/entries.php
+++ application/classes/controller/cp/entries.php
@@ -4,7 +4,8 @@ defined('SYSPATH') or die('Hacking attemp!');
 
 class Controller_CP_Entries extends Controller_Template {
 
-    public function action_index() {
+    public function action_index()
+    {
         $this->template->page_title = 'List of Entries';
         $model_for_entries = Model::factory('entry');
         $get_entries = $model_for_entries->get_entries_list();
@@ -13,7 +14,8 @@ class Controller_CP_Entries extends Controller_Template {
         $this->template->content = $view->render();
     }
 
-    public function action_edit() {
+    public function action_edit()
+    {
         $this->template->page_title = 'Edit Entry';
         $entry_id = $this->request->param('id');
         $view = View::factory('cp/entries/edit');
@@ -45,51 +47,65 @@ class Controller_CP_Entries extends Controller_Template {
         $this->template->content = $view->render();
     }
 
-    public function action_delete() {
+    public function action_delete()
+    {
+
         $this->template->page_title = 'Delete Entry';
+
         $entry_id = $this->request->param('id');
 
         if (!$entry_id) {
             throw new Exception('ID must not be empty!');
         }
-        $model_for_entries = Model::factory('entry');
-         if (!Security::check($this->request->param('id2'))) {
+
+        $entry = new Model_Entry();
+
+        if (!Security::check($this->request->param('id2'))) {
 
                 throw new HTTP_Exception_401("Bad token!");
             }
-        $delete_entry = $model_for_entries->delete_entry($entry_id);
+        $delete_entry = $entry->delete_entry($entry_id);
+
         if (!$delete_entry) {
             throw new Exception('Error with deleting entry!');
         }
+
         $this->request->redirect('cp/entries');
+
     }
 
-    public function action_write() {
+    public function action_write()
+    {
+
         $this->template->page_title = 'Write Article';
-        $model_for_users = Model::factory('user');
-        $session = Session::instance();
-        $user_session = $session->get('user');
-        $author_name = $model_for_users->get_user_by_session_id($user_session);
-        $view = View::factory('cp/entries/write');
-        $view->author = $author_name;
+
+        $user         = new Model_User();
+        $session      = Session::instance()->get('user');
+        $view         = View::factory('cp/entries/write');
+        $view->author = $users->get_user_by_session_id($session);
 
         if ($this->request->method() === Request::POST) {
+
             if (!Security::check($this->request->post('csrf_token'))) {
 
                 throw new HTTP_Exception_401("Bad token!");
             }
+
             $post_title = $this->request->post('title');
             $post_slug = $this->request->post('slug');
             $post_content = $this->request->post('content');
             $post_author = $this->request->post('author');
             $post_date = time();
+
             if (empty($post_title) and empty($post_content) and empty($post_author) and empty($post_date)) {
                 throw new Exception('Please don`t make empty fields!');
             }
-            $entry = new Model_Entry();
+
             if(empty($post_slug)){
                 $post_slug = URL::title($post_title, '_');
             }
+
+            $entry = new Model_Entry();
 
             $data         = array(
                 'title'   => $post_title,
