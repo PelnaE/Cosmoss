8e6b2922546f02dee23aa760b2b95b4414d750dd
Edgars Pelna <regative@gmail.com>
2012-08-27 14:43:47 +0300

    Adjusting code to phpcs standards - CP Controller

--- application/classes/controller/cp.php
+++ application/classes/controller/cp.php
@@ -1,43 +1,52 @@
-<?php
+<?php defined('SYSPATH') or die('Fuck you!');
 
-defined('SYSPATH') or die('Fuck you!');
+class Controller_CP extends Controller_Template
+{
 
-class Controller_CP extends Controller_Template {
-
-    public function action_index() {
+    public function action_index()
+    {
         $this->template->page_title = 'Admin CP';
-        $this->template->content = View::factory('cp/home');
+        $this->template->content    = View::factory('cp/home');
     }
 
-    public function action_sign_out() {
+    public function action_sign_out()
+    {
         $this->template->page_title = 'Logout';
-        $session = Session::instance();
-        $user_session = $session->get('user');
-        if (empty($user_session)) {
+        $session                    = Session::instance()->get('user');
+
+        if (empty($session)) {
             throw new Exception('Login to logout!');
         }
-        $session->delete('user');
+
+        Session::instance()->delete('user');
         $this->request->redirect('cp');
     }
 
-    public function action_sign_in() {
+    public function action_sign_in()
+    {
         $this->template->page_title = 'Sign In';
-        $post_nick = $this->request->post('nick');
-        $post_password = $this->request->post('password');
-        $post_cookie = $this->request->post('cookie');
-        if (empty($post_nick) and empty($post_password)) {
+
+        $post_nick                  = $this->request->post('nick');
+        $post_password              = $this->request->post('password');
+        $post_cookie                = $this->request->post('cookie');
+
+        if (empty($post_nick) && empty($post_password)) {
             throw new Exception('Please input your nick and password!');
         }
-        $model_for_user = Model::factory('user');
-        $check_user_data = $model_for_user->check_user_data($post_nick, $post_password);
+        $user            = new Model_User();
+        $check_user_data = $user->check_user_data($post_nick, $post_password);
+
         if (!$check_user_data) {
             throw new Exception('Check you login data!');
         }
+
         Session::instance()->set('user', $post_nick);
-        if(!empty($post_cookie)){
-        Cookie::set('user', $post_nick);
+
+        if (!empty($post_cookie)) {
+            Cookie::set('user', $post_nick);
         }
+
         $this->request->redirect('cp');
-    }    
+    }
 
 }
